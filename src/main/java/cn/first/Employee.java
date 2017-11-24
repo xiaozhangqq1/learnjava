d242101217448ff522180b955c189fa8003fd1d6
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-11-24 16:57:51 +0800

    变量

--- src/main/java/cn/first/Employee.java
+++ src/main/java/cn/first/Employee.java
@@ -11,6 +11,8 @@ public class Employee {
     int age;
     String destination;
     double salary;
+    public static double ss;
+    public static final String DEPARTMENT = "开发人员";
     public Employee(String name){
         this.name = name;
     }
@@ -19,5 +21,14 @@ public class Employee {
         System.out.println("年龄" + age);
         System.out.println("职位" + destination);
         System.out.println("薪水" + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("Runoob");
+        employee.setSalary(1);
+        employee.setAge(1);
+        employee.printEmployee();
+        ss = 111;
+        System.out.println(DEPARTMENT + "平均工资" + ss);
     }
 }
---
+++ src/main/java/cn/first/className.java
@@ -0,0 +1,18 @@
+package cn.first;
+
+/**
+ * Created by zhangxuelong on 2017/11/24
+ */
+public class className {
+    private boolean myFlag;
+    static final double weeks = 9.5;
+    protected static final int BOXWINDTH = 42;
+    String version = "1.4.5";
+    boolean processOrder(){
+        return true;
+    }
+    public static void main(String[] args) {
+        // 方法体
+    }
+}
+
