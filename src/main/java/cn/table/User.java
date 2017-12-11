8dc7e2336c5cf06b2ad6cbbb34f3002777b6a890
xiaozhangqq1 <xiaoqianqq1@gmail.com>
2017-12-11 09:04:31 +0800

    用户

---
+++ src/main/java/cn/table/User.java
@@ -0,0 +1,9 @@
+package cn.table;
+
+public class User {
+    private String uid;
+    private String user_type;
+    private String user_industry;
+    private String level;
+    private String gender;
+}
