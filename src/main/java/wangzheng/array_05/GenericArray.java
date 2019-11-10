b34824ae4402aac70d77edb451ce571e6eef8a2b
zhangxuelong10 <xuelongzhang@vivedu.com>
2019-11-10 15:43:45 +0800

    update array.

--- src/main/java/wangzheng/array_05/GenericArray.java
+++ src/main/java/wangzheng/array_05/GenericArray.java
@@ -38,21 +38,21 @@ public class GenericArray<T> {
     }
 
     // 修改index元素的位置
-    public void set(int index,T e){
-        checkIndex(index);;
+    public void set(int index, T e) {
+        checkIndex(index);
         data[index] = e;
     }
 
     // 获取对应index位置的元素
-    public T get(int index){
-        checkIndex(index);;
+    public T get(int index) {
+        checkIndex(index);
         return data[index];
     }
 
     // 查看数组是否包含元素e
-    public boolean contains(T e){
-        for(int i = 0;i<size;i++){
-            if(data[i].equals(e)){
+    public boolean contains(T e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
                 return true;
             }
         }
@@ -60,9 +60,9 @@ public class GenericArray<T> {
     }
 
     // 获取对应元素的下标，未找到，返回-1
-    public int find(T e){
-        for(int i = 0;i<size;i++){
-            if (data[i].equals(e)){
+    public int find(T e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
                 return i;
             }
         }
@@ -70,15 +70,77 @@ public class GenericArray<T> {
     }
 
     // 在index位置，插入元素e，时间复杂度O(m+n)
-    public void add(int index,T e){
+    public void add(int index, T e) {
         checkIndex(index);
         // 如果当前元素个数等于数组容量，则将数组扩容为原来的2倍
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
 
+    // 向数组头插入元素
+    public void addFirst(T e) {
+        add(0, e);
+    }
+
+    // 向数组尾插入元素
+    public void addLast(T e) {
+        add(size, e);
+    }
+
+    // 删除index位置的元素，并返回
+    public T remove(int index) {
+        checkIndexForRemove(index);
+        T ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+        // 缩容
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Array size = %d,capacity = %d \n,size,data.length"));
+        builder.append('[');
+        for (int i = 0; i < size; i++) {
+            builder.append(data[i]);
+            if (i != size - 1) {
+                builder.append(",");
+            }
+            builder.append(']');
+        }
+        return builder.toString();
+    }
+
+    // 扩容方法，时间复杂度O(n)
+    private void resize(int capacity) {
+        T[] newData = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
     }
 
     private void checkIndex(int index) {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed! Require index >= 0 and index <= size.");
+        }
+    }
+
+    private void checkIndexForRemove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("remove failed,Require index >=0 and index <= size.");
         }
     }
 }
